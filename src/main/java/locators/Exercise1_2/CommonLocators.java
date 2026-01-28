2d292a965c5f53b9cc3e8cb199b4dd9fa1e36ff5
ThanhLam222 <nguyenthanhlam222@gmail.com>
2026-01-28 09:31:40 +0700

    fix: resolve CI style issues

--- src/main/java/locators/Exercise1_2/CommonLocators.java
+++ src/main/java/locators/Exercise1_2/CommonLocators.java
@@ -2,10 +2,10 @@ package locators.Exercise1_2;
 
 import org.openqa.selenium.By;
 
-    public class CommonLocators {
-        // title of a page - Strategies: tagname
-        public static final By PAGE_TITLE = By.tagName("h1");
+public class CommonLocators {
+    // title of a page - Strategies: tagname
+    public static final By PAGE_TITLE = By.tagName("h1");
 
-        // normalize-space()
-        public static final By ELEMENTS_NORMALIZE = By.xpath("//div[normalize-space()='Elements']");
-    }
+    // normalize-space()
+    public static final By ELEMENTS_NORMALIZE = By.xpath("//div[normalize-space()='Elements']");
+}
