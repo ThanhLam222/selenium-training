4c6a65625a36bb66505dd375262b51dc2b1cd786
ThanhLam222 <nguyenthanhlam222@gmail.com>
2026-02-07 16:40:00 +0700

    fix: check style error - unused import

--- src/main/java/pom/exercise3_2/PracticeFormPage.java
+++ src/main/java/pom/exercise3_2/PracticeFormPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-import utils.ConfigReader;
 
 public class PracticeFormPage extends BasePage {
     private final By firstNameInp = By.id("firstName");
